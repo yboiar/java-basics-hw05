0a799790cd954b4d38147f163ac836c5d48bab28
Yuriy Boiar <yuraboiar@gmail.com>
2020-03-10 16:34:49 +0200

    05 hw draft solution
    (cherry picked from commit bc5034c95c5cf7ae7229e2113fd826c59e97c3e1)

--- src/Eagle.java
+++ src/Eagle.java
@@ -1,5 +1,10 @@
-public class Eagle {
+public class Eagle extends FlyingBird {
   // TODO fix class declaration
 
   // TODO override toString method by returning a string with the class name
+
+    @Override
+    public String toString() {
+        return Eagle.class.getSimpleName();
+    }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -7,9 +7,12 @@ public class Zoo {
    *
    * @param list list of birds of particular type
    */
-  public static void feed(List list) {
+  public static void feed (List<? extends Bird> list) {
     // TODO fix method declaration
-    System.out.println("Feeding birds");
+
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println("Feeding birds: " + list.get(i).getClass().getSimpleName());
+    }
     // TODO print birds in the cycle
   }
 
@@ -19,9 +22,12 @@ public class Zoo {
    * @param list list of birds of particular type
    * @param bird new bird
    */
-  public static void acceptBird(List list, FlyingBird bird) {
+  public static void acceptBird(List<FlyingBird> list, FlyingBird bird) {
     // TODO fix method declaration
+    bird.checkWings();
+    list.add(bird);
     System.out.println("Accepting bird to a section");
+
     // TODO check flying bird wings
     // TODO add bird to the list and print it
   }
@@ -32,10 +38,15 @@ public class Zoo {
    * @param list    list of birds
    * @param newBird bird to add
    */
-  public static void registerBird(List list, Bird newBird) {
+  public static void registerBird(List<Bird> list, Bird newBird) {
     // TODO fix method declaration
     System.out.println("Adding bird to the birds list");
+
     // TODO add bird to list
+    list.add(newBird);
+    for (Bird bird : list) {
+      System.out.println(bird.toString());
+    }
     // TODO print birds in the cycle
   }
 }
